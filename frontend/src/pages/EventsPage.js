1dcd80b65ffa402821810d34574db583741aae03
Hadeer Salah <91800040+hadeersalah7@users.noreply.github.com>
2024-07-02 09:40:51 +0000

    updates on event pages

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -7,12 +7,15 @@ const Events = [
 ];
 export default function EventsPage() {
   return (
+    <>
+    <h1>Event Page</h1>
     <ul>
       {Events.map((event) => (
         <li key={event.id}>
-          <Link to={`/events/${event.id}`}>{event.title}</Link>
+          <Link to={event.id}>{event.title}</Link>
         </li>
       ))}
     </ul>
+    </>
   );
 }
