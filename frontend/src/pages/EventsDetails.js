8c6425e664530caef7a4a09e163455f44003aaf5
Hadeer Salah <91800040+hadeersalah7@users.noreply.github.com>
2024-07-03 05:51:38 +0000

    adding a new EventRoot layout

--- frontend/src/pages/EventsDetails.js
+++ frontend/src/pages/EventsDetails.js
@@ -2,10 +2,5 @@ import React from "react";
 import { useParams } from "react-router-dom";
 export default function EventsDetails() {
   const eventId = useParams();
-  return (
-    <>
-      <h2>Event Detail Page</h2>
-      <p>Event ID is: {eventId.id}</p>
-    </>
-  );
+  return <div>{eventId.title}</div>;
 }
