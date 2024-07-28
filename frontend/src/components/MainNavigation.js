2c6bb533df8ee4e03c2f2a931d320759aa724e41
hadeersalah7 <hadeermhmd70@gmail.com>
2024-07-28 14:19:46 +0300

    adding routes

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -30,7 +30,7 @@ function MainNavigation() {
 
           <li>
             <NavLink
-              to="/auth"
+              to="/auth?mode=login"
               className={({ isActive }) =>
                 isActive ? classes.active : undefined
               }
--- frontend/src/pages/AuthenticationPage.js
+++ frontend/src/pages/AuthenticationPage.js
@@ -1,3 +1,4 @@
+import { json, redirect } from "react-router-dom";
 import AuthForm from "../components/AuthForm";
 
 function AuthenticationPage() {
@@ -5,3 +6,32 @@ function AuthenticationPage() {
 }
 
 export default AuthenticationPage;
+
+export async function action({ request }) {
+    const searchParams = new URL(request.url).searchParams
+    const mode = searchParams.get("mode") || "login"
+    if (mode !== "login" && mode !== "signup") {
+        throw json({ message: "unsupported mode" }, { status: 422 })
+    }
+    const response = await request.formData()
+
+    const authData = {
+        email: response.get("email"),
+        password: response.get("password")
+    }
+
+    const data = await fetch("http://localhost:8080" + mode, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(authData)
+    })
+
+    if (data.status === 422 || data.status === 401) {
+        return json
+    }
+
+    if (!data.ok) {
+        throw json({ message: "Couldn't authnticate user." }, { status: 500 })
+    }
+    redirect("/")
+}
